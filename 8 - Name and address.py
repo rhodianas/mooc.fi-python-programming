485d7a14e4e0bbd37cc16756ab04a40642073c49
rhodianas <114800532+rhodianas@users.noreply.github.com>
2022-10-12 21:53:37 +0300

    Solution
    Exercise 8: write a program which asks for the user's name and address

---
+++ 8 - Name and address.py
@@ -0,0 +1,8 @@
+# Write your solution here
+givenName = input("Given name:")
+familyName = input("Family name:")
+address = input("Street address:")
+cityAndPostalCode = input("City and postal code:")
+print(givenName + " " + familyName)
+print(address)
+print(cityAndPostalCode)
