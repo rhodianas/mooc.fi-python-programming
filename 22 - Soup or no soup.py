e2096d0cd38f0dd8607945a9278c375b1e5b24c8
rhodianas <114800532+rhodianas@users.noreply.github.com>
2022-10-17 15:46:44 +0300

    Solution
    Exercise 22: write a program which asks for the user's name. If the name is anything but "Jerry", the program then asks for the number of portions and prints out the total cost. The price of a single portion is 5.90.

---
+++ 22 - Soup or no soup.py
@@ -0,0 +1,6 @@
+# Write your solution here
+name = input("Please tell me your name:")
+if (name != "Jerry"):
+    portions = int(input("How many portions of soup?"))
+    print(f"The total cost is {5.90*portions}")
+print("Next please!")
