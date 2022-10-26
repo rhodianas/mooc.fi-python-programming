bf06c5a29f4c0c85edbe8778faa684ed2de6f678
rhodianas <114800532+rhodianas@users.noreply.github.com>
2022-10-26 23:43:22 +0300

    Solution
    Exercise 65: write a program which asks the user for strings using a loop. The program prints out each string underlined as shown in the examples below. The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.

---
+++ 65 - Underlining.py
@@ -0,0 +1,6 @@
+# Write your solution here
+string = input("Please type in a string:")
+while len(string) > 0:
+    print(string)
+    print("-"*len(string)+"\n")
+    string = input("Please type in a string:")
