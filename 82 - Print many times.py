2c8e8f1c4b13aaeb087cec087f498b6a49ad6cda
rhodianas <114800532+rhodianas@users.noreply.github.com>
2022-10-31 23:23:57 +0200

    Solution
    Exercise 82: write a function named print_many_times(text, times), which takes a string and an integer as arguments. The integer argument specifies how many times the string argument should be printed out.

---
+++ 82 - Print many times.py
@@ -0,0 +1,8 @@
+# Write your solution here
+def print_many_times(input_string, number):
+    print((input_string+"\n")*number)
+
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    print_many_times("python", 5)
