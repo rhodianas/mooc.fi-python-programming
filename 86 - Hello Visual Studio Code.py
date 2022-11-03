4a2ac676b4771cbb7358cecdb4b5b7e1d1314d9d
rhodianas <114800532+rhodianas@users.noreply.github.com>
2022-11-03 23:27:33 +0200

    Solution
    Exercise 86: write a program which asks the user which editor they are using. The program should keep on asking until the user types in Visual Studio Code.

---
+++ 86 - Hello Visual Studio Code.py
@@ -0,0 +1,10 @@
+# Write your solution
+while True:
+    editor = input("Editor:")
+    if editor.lower() == "notepad" or editor.lower() == "word":
+        print("awful")
+    elif editor.lower() == "visual studio code":
+        print("an excellent choice!")
+        break
+    else:
+        print("not good")
